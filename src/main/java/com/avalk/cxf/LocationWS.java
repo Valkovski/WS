0a8ec4e974b47656b458b6f4b9fe0356802aebbc
Andrii_Valkovskyi <Anti17Air96>
2016-12-07 15:45:32 +0200

    new

--- src/main/java/com/avalk/cxf/LocationWS.java
+++ src/main/java/com/avalk/cxf/LocationWS.java
@@ -10,16 +10,16 @@ import javax.xml.ws.ResponseWrapper;
 /**
  * Created by Abrams on 19.07.2016.
  */
-@WebService(endpointInterface = "com.avalk.cxf.ILocationsWS",
-        serviceName = "Locations", targetNamespace = "http://avalk.com", portName = "8080")
+@javax.jws.WebService(portName = "8080", serviceName = "SOAPService",
+        targetNamespace = "http://avalk.com/",
+        endpointInterface = "com.avalk.cxf.ILocationsWS")
 @SOAPBinding(style = SOAPBinding.Style.RPC, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED, use = SOAPBinding.Use.LITERAL)
 public class LocationWS implements ILocationsWS {
 
     @WebMethod(operationName = "EchoMessageTest")
     @RequestWrapper(targetNamespace="http://avalk.com/types",
             className="java.lang.String")
-    @ResponseWrapper(targetNamespace="http://avalk.com/types",
-            className="org.eric.demo.Quote")
+
     public String basicMessage(@WebParam(name = "message", mode = WebParam.Mode.IN) String message) {
         String result = "";
         result = "testHello" + message;
--- src/main/java/com/avalk/cxf/WebServiceConfig.java
+++ src/main/java/com/avalk/cxf/WebServiceConfig.java
@@ -3,35 +3,52 @@ package com.avalk.cxf;
 /**
  * Created by Abrams on 27.11.2016.
  */
-import javax.xml.ws.Endpoint;
+
 import org.apache.cxf.Bus;
+import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.endpoint.Server;
-import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.avalk.cxf.LocationWS;
+import org.springframework.context.annotation.ImportResource;
 
-import java.util.Arrays;
+import javax.xml.ws.Endpoint;
+import javax.xml.ws.soap.SOAPBinding;
 
 
 @Configuration
+@EnableAutoConfiguration
+@ImportResource({"classpath:META-INF/cxf-servlet.xml"})
 public class WebServiceConfig {
     @Autowired
-    private Bus bus;
+    private ApplicationContext applicationContext;
 
-    public static void main(String[] args) {
-        SpringApplication.run(LocationWS.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Server ready...");
+       // Thread.sleep(5 * 60 * 1000);
+        System.out.println("Server exiting");
+        SpringApplication.run(WebServiceConfig.class, args);
+    }
+    protected WebServiceConfig() throws Exception {
+        System.out.println("Starting Server");
+//        SOAPBinding binding = (SOAPBinding) endpoint().getBinding();
+//        binding.setMTOMEnabled(true);
     }
 
     @Bean
-    public Server rsServer() {
-        JAXRSServerFactoryBean endpoint = new JAXRSServerFactoryBean();
-        endpoint.setBus(bus);
-        endpoint.setAddress("/");
-        endpoint.setServiceBeans(Arrays.<Object>asList(new LocationWS()));
-        return endpoint.create();
+    public Endpoint endpoint() {
+        LocationWS implementor = new LocationWS();
+        EndpointImpl ep = new EndpointImpl(springBus(), implementor);
+        ep.publish("/location");
+        return ep;
+    }
+
+    @Bean(name= Bus.DEFAULT_BUS_ID)
+    public Bus springBus() {
+        return new SpringBus();
     }
 }
